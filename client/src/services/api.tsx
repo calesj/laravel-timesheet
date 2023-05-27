c0de4e1bffd99db69add32581db8fc70db88f123
Cales <86434489+calesj@users.noreply.github.com>
2023-05-27 10:52:19 -0300

    feat - aqui estamos utilizando o arquivo `axios`, chamando ele, mas sem passar nenhum contexto como parametro

--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -1,17 +1,5 @@
-import axios from "axios"
-import {parseCookies} from 'nookies'
+import {getAPIClient} from "@/services/axios";
 
-// PEGA TODOS OS TOKENS DO BROWSER
-const token = parseCookies()
-
-// CRIAMOS UMA CONSTANTE QUE FAZ UMA REQUISICAO NA URL PADRAO DA API
-export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/'
-})
-
-// VERIFICA SE EXISTE UM TOKEN CHAMADO `m2_token`
-if (token.m2_token) {
-    // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FA;A
-    // JA ENVIE O TOKEN AUTOMATICAMENTE
-    api.defaults.headers['Authorization'] = `Bearer ${token.m2_token}`;
-}
+// UTILIZAMOS ESSE METODO PRA FAZER CHAMADAS APARTIR DO BROWSER
+// JA QUE NAO ESTAMOS PASSANDO NENHUM `ctx` COMO PARAMETRO
+export const api = getAPIClient()
