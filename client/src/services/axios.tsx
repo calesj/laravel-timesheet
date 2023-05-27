426670cd79a612c28a6844fc7828ecd86a160925
Cales <86434489+calesj@users.noreply.github.com>
2023-05-27 10:46:57 -0300

    feat - componente pra ajudar nas requisicoes futuras, ele pode receber como parametro um ctx, caso queiramos fazer requisicoes pelo lado do servidor do next

---
+++ client/src/services/axios.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {parseCookies} from "nookies";
+
+export function getAPIClient(ctx?: any) {
+
+    // PEGA TODOS OS TOKENS DO BROWSER
+    const token = parseCookies(ctx)
+
+// CRIAMOS UMA CONSTANTE QUE FAZ UMA REQUISICAO NA URL PADRAO DA API
+    const api = axios.create({
+        baseURL: 'http://127.0.0.1:8000/api/'
+    })
+
+// VERIFICA SE EXISTE UM TOKEN CHAMADO `m2_token`
+    if (token.m2_token) {
+        // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FA;A
+        // JA ENVIE O TOKEN AUTOMATICAMENTE
+        api.defaults.headers['Authorization'] = `Bearer ${token.m2_token}`;
+    }
+
+    return api;
+}
