fd060a59452fd253b87ccc93c591438f354836d3
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 14:28:38 -0300

    feat - criando a pagina create, das escalas, responsavel por fazer o cadastro de escalas

---
+++ client/src/pages/escalas/create.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+export default function create() {
+    return (
+        <div>
+            {
+                // COMPONENTE HEADER DA PAGINA
+            }
+            <Header/>
+        </div>
+    )
+}
