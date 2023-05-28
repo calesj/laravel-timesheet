4284584cc3fdbee6671368810270ecef5a4ea02f
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 18:15:04 -0300

    feat - adicionando as rotas referente ao controller CollaboratorController.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -18,7 +18,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// ROTAS DO HORARIO DE ESCALA
 Route::middleware('auth:sanctum')->controller(\App\Http\Controllers\TimescaleController::class)->prefix('timescale')->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
+
+// ROTAS DO COLABORADOR
+Route::middleware('auth:sanctum')->controller(\App\Http\Controllers\CollaboratorController::class)->prefix('collaborator')->group(function () {
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
     Route::post('/', 'store');
