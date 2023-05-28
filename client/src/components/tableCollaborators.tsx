ccae59416485bac991d92fda7eaff75b4b452d26
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 13:41:33 -0300

    feat - renomeando o componente, table, para tableCollaborators.tsx

--- client/src/components/tableCollaborators.tsx
+++ client/src/components/tableCollaborators.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Table() {
+export default function TableCollaborators() {
     return (
         <div className="flex flex-col">
             <div className="overflow-x-auto">
