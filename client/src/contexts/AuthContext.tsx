0b59e004b0ab80633ba3e0f2c0745df33705c083
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 12:04:20 -0300

    feat - adicionando o metodo `getUser` responsavel por verificar se ja existe alguem logado, adicionando o metodo `registerIn`, metodo responsavel por fazer a requisicao na API para criacao de conta

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,15 +1,24 @@
-import {createContext, useEffect, useState} from "react"
+import {createContext, Dispatch, useEffect, useState} from "react"
 import {useRouter} from "next/router";
-import {parseCookies, setCookie} from 'nookies'
+import {destroyCookie, parseCookies, setCookie} from 'nookies'
 import axios from "axios"
 import {api} from "@/services/api";
 
+// TIPAGEM DO USUARIO
 type User = {
     name: string
     email: string,
 }
 
+// TIPAGEM DE LOGIN
 type SignInData = {
+    email: string;
+    password: string;
+}
+
+// TIPAGEM DE REGISTRO
+type registerInData = {
+    name: string,
     email: string;
     password: string;
 }
@@ -17,7 +26,10 @@ type SignInData = {
 type AuthContextType = {
     isAuthenticated: boolean;
     user: User
+    getUser: any
     signIn: (data: SignInData) => Promise<void>
+    signOut: any
+    registerIn: (data: registerInData) => Promise<void>
 }
 
 
@@ -32,26 +44,87 @@ export function AuthProvider({children}) {
 
     const router = useRouter()
 
+    // METODO RESPONSAVEL, POR VERIFICAR SE JA EXISTE ALGUEM AUTENTICADO
+    // SE TIVER, ELE REDIRECIONA PARA A PAGINA DE DASHBOARD
+    // E SETA AS INFORMACOES DO USUARIO
+    async function getUser(route = null) {
+        const token = parseCookies()
+        if (token.m2_token) {
+            let response =  await api.get('/user')
+                .then((response => {
+                    return response
+                }))
+                .catch((e => {
+                    // DESTROI O COOKIE
+                    destroyCookie(undefined, 'm2_token') // DESTROI O COOKIE
+                }));
+            // VERIFICA SE EXISTE UM id, DENTRO DO OBJETO, E SE ELE NAO ESTA VAZIO
+            if (response?.data.id && response.data.id !== '') {
+                setUser(response.data)
+
+                // VERIFICA SE ESTAO PASSANDO UMA ROTA NO PARAMETRO, SE EXISTIR UMA ROTA, ELE REDIRECIONA ATE ELA
+                if (route) {
+                    router.push(`/${route}`)
+                }
+
+            } else {
+                destroyCookie(undefined, 'm2_token') // DESTROI O COOKIE, E REDIRECIONA PRA TELA DE LOGIN
+            }
+        }
+    }
+
+    // METODO RESPONSAVEL, POR REGISTRAR UMA CONTA DE USUARIO
+    async function registerIn({name, email, password}: registerInData) {
+        const response  = await axios.post('http://127.0.0.1:8000/api/register', {
+            name, email, password
+        })
+
+        if (response.data.access_token) {
+            setCookie(undefined, 'm2_token', response.data.access_token, {
+                maxAge: 60 * 29, // TEMPO DE DURACAO DO TOKEN
+            })
+
+            api.defaults.headers['Authorization'] = `Bearer ${response.data.access_token}`
+
+            setUser(response.data.user)
+
+            router.push('/dashboard')
+        }
+
+        return response
+    }
+
     // METODO RESPONSAVEL, POR LOGAR NA SUA CONTA
     async function signIn({email, password}: SignInData) {
         const response  = await axios.post('http://127.0.0.1:8000/api/login', {
           email, password
         })
 
-        setCookie(undefined, 'm2_token', response.data.access_token, {
-        maxAge: 60 * 29, // TEMPO DE DURACAO DO TOKEN
-        })
+        if (response.data.access_token) {
+            setCookie(undefined, 'm2_token', response.data.access_token, {
+                maxAge: 60 * 29, // TEMPO DE DURACAO DO TOKEN
+            })
 
-        api.defaults.headers['Authorization'] = `Bearer ${response.data.access_token}`
+            api.defaults.headers['Authorization'] = `Bearer ${response.data.access_token}`
 
-        setUser(response.data.user)
+            setUser(response.data.user)
 
-        router.push('/dashboard')
+            router.push('/dashboard')
+        }
+
+        return response;
+    }
+
+    // METODO SAIR
+    function signOut() {
+        // METODO RESPONSAVEL POR DESTRUIR O COOKIE DO TOKEN, E REDIRECIONAR PRA TELA INICIAL
+        destroyCookie(undefined, 'm2_token')
+        router.push('/')
     }
 
     return (
 
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn}}>
+        <AuthContext.Provider value={{ user, getUser, isAuthenticated, signIn, signOut, registerIn }}>
             {children}
         </AuthContext.Provider>
     )
