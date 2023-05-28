20f102df499f0730b8eb36090ebf22e6af8b1941
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 12:07:49 -0300

    feat - criando o componente que vai representar o nosso datatable

---
+++ client/src/components/table.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+
+export default function Table() {
+    return (
+        <div className="flex flex-col">
+            <div className="overflow-x-auto">
+                <div className="p-1.5 w-full inline-block align-middle">
+                    <div className="overflow-hidden border rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                            <tr>
+                                <th scope="col" className="py-3 pl-4">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            id="checkbox-all"
+                                            type="checkbox"
+                                            className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
+                                        />
+                                        <label
+                                            htmlFor="checkbox"
+                                            className="sr-only"
+                                        >
+                                            Checkbox
+                                        </label>
+                                    </div>
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                >
+                                    ID
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                >
+                                    Name
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                >
+                                    Email
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                                >
+                                    Edit
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                                >
+                                    Delete
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200">
+                            <tr>
+                                <td className="py-3 pl-4">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            type="checkbox"
+                                            className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
+                                        />
+                                        <label
+                                            htmlFor="checkbox"
+                                            className="sr-only"
+                                        >
+                                            Checkbox
+                                        </label>
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                                    1
+                                </td>
+                                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                    Jone Doe
+                                </td>
+                                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                    jonne62@gmail.com
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                    <a
+                                        className="text-green-500 hover:text-green-700"
+                                        href="#"
+                                    >
+                                        Edit
+                                    </a>
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                    <a
+                                        className="text-red-500 hover:text-red-700"
+                                        href="#"
+                                    >
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="py-3 pl-4">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            type="checkbox"
+                                            className="text-blue-600 border-gray-200 rounded focus:ring-blue-500"
+                                        />
+                                        <label
+                                            htmlFor="checkbox"
+                                            className="sr-only"
+                                        >
+                                            Checkbox
+                                        </label>
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                                    1
+                                </td>
+                                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                    Jone Doe
+                                </td>
+                                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                    jonne62@gmail.com
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                    <a
+                                        className="text-green-500 hover:text-green-700"
+                                        href="#"
+                                    >
+                                        Edit
+                                    </a>
+                                </td>
+                                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                    <a
+                                        className="text-red-500 hover:text-red-700"
+                                        href="#"
+                                    >
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
