3ec35841bd382eef550f37f37bedc3234aa06f66
Cales <86434489+calesj@users.noreply.github.com>
2023-05-28 18:16:18 -0300

    feat - adicionando comentarios no codigo, TimescaleController.php

--- server/app/Models/Timescale.php
+++ server/app/Models/Timescale.php
@@ -6,16 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ *
+ */
 class Timescale extends Model
 {
     use HasFactory;
 
+    /**
+     * CAMPOS PREENCHIVEIS
+     * @var string[]
+     */
     protected $fillable = [
         'nome',
         'escala'
     ];
 
-    // UMA ESCALA, PODE TER VARIOS COLABORADORES
+    /**
+     * UMA ESCALA, PODE TER VARIOS COLABORADORES
+     * @return HasMany
+     */
     public function collaborators(): HasMany
     {
         return $this->hasMany(Collaborator::class);
