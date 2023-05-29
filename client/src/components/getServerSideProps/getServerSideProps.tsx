2626fc3fa53e111a1088426af3f7ba9decff5b7c
Cales <86434489+calesj@users.noreply.github.com>
2023-05-29 16:46:29 -0300

    fix - melhorando o componente que coloca o token em todas es requisicoes, pra caso, de algum erro de autenticacao no axios, ele retornar pra rota de login

---
+++ client/src/components/getServerSideProps/getServerSideProps.tsx
@@ -0,0 +1,28 @@
+import {GetServerSideProps, GetServerSidePropsContext} from "next";
+import {getAPIClient} from "@/services/axios";
+import {destroyCookie, parseCookies} from "nookies";
+
+export const withAuthServerSideProps = (redirectPath = '/'): GetServerSideProps => {
+    return async (ctx: GetServerSidePropsContext) => {
+        const { req, res } = ctx;
+
+        const cookies = parseCookies({ req });
+
+        if (!cookies.m2_token) {
+            return {
+                redirect: {
+                    destination: redirectPath,
+                    permanent: false,
+                },
+            };
+        }
+
+        return {
+            props: {},
+        };
+    };
+};
+
+export const signOut = (ctx: GetServerSidePropsContext) => {
+    destroyCookie(ctx, 'm2_token');
+};
