a64fc0c6077ca75b66155c6025014a5509020ba7
Cales <86434489+calesj@users.noreply.github.com>
2023-05-29 16:50:57 -0300

    feat - se houver erro do codigo de status 401, ele vai redirecionar para a pagina de login, evitando que apareca mensagem de erros do axios para o usuario

--- client/src/services/axios.tsx
+++ client/src/services/axios.tsx
@@ -1,20 +1,28 @@
 import axios from "axios";
 import {parseCookies} from "nookies";
 
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/'
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // Redirecionar para a página de login
+            window.location.href = '/';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export function getAPIClient(ctx?: any) {
-
     // PEGA TODOS OS TOKENS DO BROWSER
-    const token = parseCookies(ctx)
+    const token = parseCookies(ctx);
 
-// CRIAMOS UMA CONSTANTE QUE FAZ UMA REQUISICAO NA URL PADRAO DA API
-    const api = axios.create({
-        baseURL: 'http://127.0.0.1:8000/api/'
-    })
-
-// VERIFICA SE EXISTE UM TOKEN CHAMADO `m2_token`
+    // VERIFICA SE EXISTE UM TOKEN CHAMADO `m2_token`
+    // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FA;A
     if (token.m2_token) {
-        // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FA;A
-        // JA ENVIE O TOKEN AUTOMATICAMENTE
         api.defaults.headers['Authorization'] = `Bearer ${token.m2_token}`;
     }
 
