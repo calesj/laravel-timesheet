70a1ef373e5065e0ff52b711dc3299bfa2e17deb
Cales <86434489+calesj@users.noreply.github.com>
2023-05-29 16:36:59 -0300

    feat - adicionando um registro padrao, a tabela users

---
+++ server/database/migrations/2023_05_29_161928_insert_user_data.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin')
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('users')->where('email', 'admin@admin.com')->delete();
+    }
+};
