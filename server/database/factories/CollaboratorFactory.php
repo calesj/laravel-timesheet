bb48d68b536524efe48204ce8f7c8fb5bedb3f33
Cales <86434489+calesj@users.noreply.github.com>
2023-05-29 16:37:23 -0300

    feat - adicionando uma factory de registros ao Collaborator

---
+++ server/database/factories/CollaboratorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Collaborator>
+ */
+class CollaboratorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => fake()->name(),
+            "matricula" => fake()->numerify('#####'),
+            "cpf" => "Cales Junes",
+            "timescale_id" => "1",
+        ];
+    }
+}
