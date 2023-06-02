d95eda01383e1829f4c1d01ba3cf875f4d0f2db6
Cales <86434489+calesj@users.noreply.github.com>
2023-06-02 18:00:57 -0300

    feat - alterando o arquivo que seta o token no header da pagina, em toda requisicao q a gente fizer

--- client/src/services/axios.tsx
+++ client/src/services/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {parseCookies} from "nookies";
+import Cookies from "js-cookie";
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/'
@@ -10,20 +10,20 @@ api.interceptors.response.use(
     (error) => {
         if (error.response && error.response.status === 401) {
             // Redirecionar para a página de login
-            window.location.href = '/';
+            //window.location.href = '/';
         }
         return Promise.reject(error);
     }
 );
 
 export function getAPIClient(ctx?: any) {
-    // PEGA TODOS OS TOKENS DO BROWSER
-    const token = parseCookies(ctx);
+    // PEGA O TOKEN DO COOKIE
+    const token = Cookies.get('m2_token');
 
     // VERIFICA SE EXISTE UM TOKEN CHAMADO `m2_token`
-    // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FA;A
-    if (token.m2_token) {
-        api.defaults.headers['Authorization'] = `Bearer ${token.m2_token}`;
+    // SE EXISTIR, ELE VAI COLOCAR ELE NO CABECALHO DA CONSTANTE `api`, FAZENDO COM QUE QUALQUER REQUISICAO QUE VOCE FAÇA
+    if (token) {
+        api.defaults.headers['Authorization'] = `Bearer ${token}`;
     }
 
     return api;
