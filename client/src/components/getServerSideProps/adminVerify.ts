f56b14ea50829b111a1524afd93575f504985a0d
Cales <86434489+calesj@users.noreply.github.com>
2023-06-02 18:11:07 -0300

    feat - alterando o arquivo, responsavel por verificar se o usuario tem privilegios de adm, no lado do servidor, ou seja antes de renderizar a pagina

---
+++ client/src/components/getServerSideProps/adminVerify.ts
@@ -0,0 +1,47 @@
+import {GetServerSideProps, GetServerSidePropsContext} from "next";
+import {destroyCookie, parseCookies} from "nookies";
+import axios from "axios";
+
+export const adminVerify = (redirectPath = '/'): GetServerSideProps => {
+    return async (ctx: GetServerSidePropsContext) => {
+        const { req, res } = ctx;
+
+        const cookies = parseCookies({ req });
+
+        if (!cookies.m2_token) {
+            return {
+                redirect: {
+                    destination: redirectPath,
+                    permanent: false,
+                },
+            };
+        }
+
+        axios.defaults.headers['Authorization'] = `Bearer ${cookies.m2_token}`
+        const response = await axios.get('http://127.0.0.1:8000/api/testeAdmin/').catch(e => {
+            return {
+                redirect: {
+                    destination: redirectPath,
+                    permanent: false,
+                },
+            };
+        })
+
+        if (!response.data?.id) {
+            return {
+                redirect: {
+                    destination: redirectPath,
+                    permanent: false,
+                },
+            };
+        }
+
+        return {
+            props: {},
+        };
+    };
+};
+
+export const signOut = (ctx: GetServerSidePropsContext) => {
+    destroyCookie(ctx, 'm2_token');
+};
