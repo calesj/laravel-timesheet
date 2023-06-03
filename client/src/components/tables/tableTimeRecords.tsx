eaa41c8629ab65c52a262bfa725558713e9841d2
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 14:31:16 -0300

    feat - removendo a opcao de deletar um registro de ponto

--- client/src/components/tables/tableTimeRecords.tsx
+++ client/src/components/tables/tableTimeRecords.tsx
@@ -18,11 +18,6 @@ export default function TableTimeRecords({ onEdit }) {
         }
     };
 
-    const collaboratorDelete = async (id) => {
-        const response = await api.delete(`/collaborator/${id}`);
-        fetchData();
-    };
-
     return (
         <div className="flex flex-col">
             <div className="overflow-x-auto">
@@ -73,12 +68,6 @@ export default function TableTimeRecords({ onEdit }) {
                                 >
                                     Edit
                                 </th>
-                                <th
-                                    scope="col"
-                                    className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
-                                >
-                                    Delete
-                                </th>
                             </tr>
                             </thead>
                             <tbody className="divide-y divide-gray-200">
@@ -109,14 +98,6 @@ export default function TableTimeRecords({ onEdit }) {
                                                 onClick={() => onEdit(item)}
                                             >
                                                 Editar
-                                            </button>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                            <button
-                                                className="text-red-300 hover:text-red-700"
-                                                onClick={() => collaboratorDelete(item.id)}
-                                            >
-                                                Excluir
                                             </button>
                                         </td>
                                     </tr>
