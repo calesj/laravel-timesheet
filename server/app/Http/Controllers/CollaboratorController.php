8f7661aed5e12dfec5232a03b6081c5df09e73d3
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 12:18:10 -0300

    feat - adicionando o metodo responsavel por buscar colaboradores no banco, atraves de algum caractere ou palavra

--- server/app/Http/Controllers/CollaboratorController.php
+++ server/app/Http/Controllers/CollaboratorController.php
@@ -71,78 +71,28 @@ class CollaboratorController extends Controller
         }
     }
 
-    /**
-     * METODO RESPONSAVEL POR FAZER ATUALIZACAO DE UM REGISTRO
-     * @param $id
-     * @param Request $request
-     * @return JsonResponse|true
-     */
-    public function update($id, Request $request)
+    public function search(string $busca)
     {
         if (!Auth::check()) {
             return response()->json(['Unauthorized'], 401);
         }
 
-        $validate = FormValidation::validar($request->all(), $this->rules);
-
-        // VERIFICA SE EXISTE ALGUM ERRO DE VALIDACAO, SE EXISTIR ELE RETORNA ESSE ERRO
-        if ($validate !== true) {
-            return $validate;
-        }
-
         try {
-            $collaborator = Collaborator::find($id);
+            $collaborators = Collaborator::with(['user', 'timescale'])
+                ->whereHas('user', function ($query) use ($busca) {
+                    $query->where('name', 'LIKE', '%' . $busca . '%');
+                })
+                ->orWhere('matricula', 'LIKE', '%' . $busca . '%')
+                ->orWhere('cpf', 'LIKE', '%' . $busca . '%')
+                ->get();
 
-            // CASO NAO EXISTA ESSE USUARIO
-            if (!$collaborator) {
-                return response()->json('Recurso nao encontrado', 204);
+            if ($collaborators->isEmpty()) {
+                return response()->json(['Recurso nao encontado'], 204);
             }
 
-            $collaborator->nome = $request['nome'];
-            $collaborator->matricula = $request['matricula'];
-            $collaborator->cpf = $request['cpf'];
-            $collaborator->timescale_id = $request['timescale_id'];
-            $timescale = Timescale::find($request['timescale_id']);
-
-            // VERIFICA SE O ID DO TIMESCALE, EXISTE NA TABELA timescale
-            if ($timescale) {
-                $collaborator->save();
-                return response()->json($collaborator);
-            } else {
-                $validate = [
-                    'errors' => [
-                        'timescale_id' => 'Por favor, escolha um horario de escala valido'
-                    ]
-                ];
-                return response()->json($validate);
-            }
-
-        } catch (\Exception $e) {
-            return response()->json($e);
-        }
-    }
-
-    /**
-     * METODO RESPONSAVEL POR DELETAR O REGISTRO DA TABELA COLLABORATORS
-     * @param $id
-     * @return JsonResponse
-     */
-    public function destroy($id)
-    {
-        if (!Auth::check()) {
-            return response()->json(['Unauthorized'], 401);
-        }
-
-        $collaborator = Collaborator::find($id);
-        if (!$collaborator) {
-            return response()->json('Recurso nao encontrado', 204);
-        }
-
-        try {
-            $collaborator->delete();
-            return response()->json(['message' => 'Item deletado com sucesso']);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Desculpe, algo deu errado']);
+            return response()->json($collaborators);
+        } catch (\Exception $exception) {
+            return response()->json($exception);
         }
     }
 }
