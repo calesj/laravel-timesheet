c2c8395bb3f7fe9a88402de0641d08a66df2a27f
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 14:28:31 -0300

    feat - removendo alguns testes de rotas que nao estao mais na aplicacao

--- server/tests/Feature/CollaboratorsTest.php
+++ server/tests/Feature/CollaboratorsTest.php
@@ -57,51 +57,6 @@ class CollaboratorsTest extends TestCase
     }
 
     /**
-     * VERIFICA SE O USUARIO PODE ACESSAR A ROTA DO METODO store() DO COLLABORATOR, SEM ESTAR AUTENTICADO
-     * @return void
-     */
-    public function test_user_auth_collaborator_store()
-    {
-        $response = $this->postJson('/api/collaborator/', [
-            [
-                'nome' => 'nome Teste1',
-                'matricula' => 'escala teste1',
-                'cpf' => 'cpf teste',
-                'timescale_id' => '1',
-            ]
-        ]);
-
-        $response->assertStatus(401);
-    }
-
-    /**
-     * VERIFICA SE O USUARIO PODE ACESSAR A ROTA DO METODO show() DO COLLABORATOR, SEM ESTAR AUTENTICADO
-     * @return void
-     */
-    public function test_user_auth_collaborator_update()
-    {
-        $response = $this->putJson('/api/collaborator/1', [
-                'nome' => 'nome Teste1',
-                'matricula' => 'escala teste1',
-                'cpf' => 'cpf teste',
-                'timescale_id' => '1',
-        ]);
-
-        $response->assertStatus(401);
-    }
-
-    /**
-     * VERIFICA SE O USUARIO PODE ACESSAR A ROTA DO METODO delete() DO COLLABORATOR, SEM ESTAR AUTENTICADO
-     * @return void
-     */
-    public function test_user_auth_collaborator_delete()
-    {
-        $response = $this->deleteJson('/api/collaborator/1');
-
-        $response->assertStatus(401);
-    }
-
-    /**
      * VERIFICA SE O USUARIO AUTENTICADO PODE ACESSAR A ROTA DO METODO index() DO COLLABORATOR
      * VERIFICA SE VAI RETORNAR A QUANTIDADE CERTA DE REGISTROS
      * @return void
@@ -114,7 +69,7 @@ class CollaboratorsTest extends TestCase
 
         $response->assertStatus(200);
 
-        $response->assertJsonCount(10); // Verifica se há 10 registros na resposta JSON
+        $response->assertJsonCount(11); // Verifica se há 11 registros na resposta JSON
     }
 
     /**
@@ -129,65 +84,6 @@ class CollaboratorsTest extends TestCase
         $response->assertStatus(200);
         $response->assertJson([
             'id' => 15
-        ]);
-    }
-
-    /**
-     * VERIFICA SE O USUARIO AUTENTICADO PODE ACESSAR A ROTA DO METODO store() DO COLLABORATOR
-     * E SE VAI RETORNAR O REGISTRO CERTO
-     * @return void
-     */
-    public function test_collaborator_store()
-    {
-        $response = $this->authenticateUser()->postJson('/api/collaborator/', [
-            'nome' => 'nome Teste1',
-            'matricula' => 'escala teste1',
-            'cpf' => 'cpf teste',
-            'timescale_id' => '1',
-        ]);
-        $response->assertStatus(200);
-        $response->assertJson([
-            'nome' => 'nome Teste1',
-            'matricula' => 'escala teste1',
-            'cpf' => 'cpf teste',
-            'timescale_id' => '1',
-        ]);
-    }
-
-    /**
-     * VERIFICA SE O USUARIO AUTENTICADO PODE ACESSAR A ROTA DO METODO update() DO COLLABORATOR
-     * E SE VAI RETORNAR O REGISTRO CERTO
-     * @return void
-     */
-    public function test_collaborator_update()
-    {
-        $response = $this->authenticateUser()->putJson('/api/collaborator/1', [
-            'nome' => 'nome Teste1',
-            'matricula' => 'escala teste1',
-            'cpf' => 'cpf teste',
-            'timescale_id' => '1',
-        ]);
-        $response->assertStatus(200);
-        $response->assertJson([
-            'id' => 1,
-            'nome' => 'nome Teste1',
-            'matricula' => 'escala teste1',
-            'cpf' => 'cpf teste',
-            'timescale_id' => '1',
-        ]);
-    }
-
-    /**
-     * VERIFICA SE O USUARIO AUTENTICADO PODE ACESSAR A ROTA DO METODO delete() DO COLLABORATOR
-     * E SE VAI RETORNAR O REGISTRO CERTO
-     * @return void
-     */
-    public function test_collaborator_delete()
-    {
-        $response = $this->authenticateUser()->deleteJson('api/collaborator/1');
-        $response->assertStatus(200);
-        $response->assertJson([
-            "message" => "Item deletado com sucesso"
         ]);
     }
 }
