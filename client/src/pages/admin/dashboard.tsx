6937877745512db61cb99dcd7dfde39313b6b683
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 12:18:22 -0300

    feat - tentando deixar o dashboard mais responsivo

--- client/src/pages/admin/dashboard.tsx
+++ client/src/pages/admin/dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
 
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                    <h1 className="text-3xl font-bold text-gray-900">Todos os Funcionarios Cadastrados - ADMIN</h1>
                 </div>
             </header>
             <main>
