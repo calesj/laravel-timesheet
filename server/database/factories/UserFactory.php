5f0c144cf29135772acfc92d19a0154a9bc20af4
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 14:33:05 -0300

    feat - alterando o factory de usuarios, pra que todos tenham uma senha padrao

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -20,7 +20,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('12345678'), // password
+            'user_privilege_id' => 1
         ];
     }
 
