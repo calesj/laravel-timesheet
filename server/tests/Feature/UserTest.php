89a149bf4df64ca63e58000d8d043a0cee4d9dc3
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 14:32:41 -0300

    feat - adicionando testes, relacionados a administracao

--- server/tests/Feature/UserTest.php
+++ server/tests/Feature/UserTest.php
@@ -6,6 +6,37 @@ class UserTest extends TestCase
 {
     use RefreshDatabase;
     /**
+     * FAZ O LOGIN DE USUARIO COM PRIVILEGIOS DE ADMINISTRADOR
+     */
+    private function authenticateUser()
+    {
+        $user = $this->postJson('api/login', [
+            'email' => 'admin@admin.com',
+            'password' => 'admin'
+        ]);
+
+        return $this->withHeaders([
+            'Authorization' => 'Bearer ' . $user['access_token'],
+        ]);
+    }
+
+    /**
+     * FAZ O LOGIN DE UM USUARIO SEM PRIVILEGIO DE ADMINISTRADOR
+     * @return UserTest
+     */
+    private function authenticateUserNoAdmin()
+    {
+        $user = $this->postJson('api/login', [
+            'email' => 'teste@teste.com',
+            'password' => '12345678'
+        ]);
+
+        return $this->withHeaders([
+            'Authorization' => 'Bearer ' . $user['access_token'],
+        ]);
+    }
+
+    /**
      * TESTA SE A ROTA DE LOGIN ESTA FUNCIONANDO
      * @return void
      */
@@ -32,5 +63,65 @@ class UserTest extends TestCase
         $response = $this->getJson('/api/user');
 
         $response->assertStatus(401);
+    }
+
+    /**
+     * VERIFICA SE O USUARIO NAO AUTENTICADO CONSEGUE FAZER O REGISTRO DE OUTRO USUARIO
+     * @return void
+     */
+    public function test_user_auth_register()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'tales teste',
+            'email' => 'tales@email.com',
+            'password' => '12345678',
+            'cpf' => 'teste',
+            'matricula' => 'teste',
+            "timescale_id" => "1"
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    /**
+     * VERIFICA SE O USUARIO AUTENTICADO, MAS SEM PRIVILEGIOS DE ADMINISTRADOR, CONSEGUE CRIAR UM USUARIO
+     * @return void
+     */
+    public function test_user_auth_register_admin()
+    {
+        $response = $this->authenticateUserNoAdmin()->postJson('/api/register', [
+            'name' => 'tales teste',
+            'email' => 'tales@email.com',
+            'password' => '12345678',
+            'cpf' => 'teste',
+            'matricula' => 'teste',
+            "timescale_id" => "1"
+        ]);
+
+        $response->assertStatus(403);
+        $response->assertJson(['message' => "acesso negado"]);
+    }
+
+    /**
+     * VERIFICA SE O USUARIO ADMINISTRADOR CONSEGUE FAZER O REGISTRO DE OUTRO USUARIO
+     * @return void
+     */
+    public function test_user_register()
+    {
+        $response = $this->authenticateUser()->postJson('/api/register', [
+            'name' => 'tales teste',
+            'email' => 'tales@email.com',
+            'password' => '12345678',
+            'cpf' => 'teste',
+            'matricula' => 'teste',
+            "timescale_id" => "1"
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'cpf' => 'teste',
+            'matricula' => 'teste',
+            "timescale_id" => "1"
+        ]);
     }
 }
