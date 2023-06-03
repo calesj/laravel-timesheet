1d7f92d4227e1b934bc504d42f63a4b9b83b82c0
Cales <86434489+calesj@users.noreply.github.com>
2023-06-03 14:28:56 -0300

    feat - removendo defaultValue, da senha

--- client/src/pages/admin/dashboard.tsx
+++ client/src/pages/admin/dashboard.tsx
@@ -148,7 +148,6 @@ export default function Dashboard() {
                                             required
                                             className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                             placeholder="Senha do colaborador"
-                                            defaultValue={collaborator.password ? collaborator.password: '' }
                                         />
                                     </div>) : ''
                                     }
